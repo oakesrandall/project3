b1385e9e1b634fd4b6e5b2241a9183344788ee9f
alexliinc <alexli@indiana.edu>
2017-08-24 11:54:24 -0600

    seed updated

---
+++ src/db/seed.ts
@@ -0,0 +1,20 @@
+import { db } from '../models';
+let DB = db.models;
+
+var restaurantDummy = [
+	{
+    name: "burger shack",
+    url: "www.b.com",
+    address: "123 b st"
+	},
+  {
+    name: "taco truck",
+    url: "www.t.com",
+    address: "456 t dr"
+	},
+  {
+    name: "mcbangers",
+    url: "www.m.com",
+    address: "789 m ave"
+	}
+];
