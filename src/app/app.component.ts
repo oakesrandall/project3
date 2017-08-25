7eaf9cf91c7bc9c9fc4c2fa5a27ede64ccc67fda
m2creger <marshall.creger@gmail.com>
2017-08-25 13:46:20 -0600

    trying to merge

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
 import * as firebase from 'firebase';
 import { ApiKeyService } from '../app/apikey.service';
-=======
 
 
->>>>>>> 0d767039b5a90d10098f97e04aa759beca71a2ce
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
