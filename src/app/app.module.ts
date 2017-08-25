599e73e794652055a048c5f95d64e45ebc6206b5
David_Randall <oakesrandall@gmail.com>
2017-08-25 11:28:39 -0600

    added landing and about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
+import { AboutComponent } from './about/about.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ImageComponent
+    ImageComponent,
+    AboutComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule
