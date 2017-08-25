87beb7c4721ef85bafd06fe80c8f29f7186b3be2
RettBehrens <everettbehrens@gmail.com>
2017-08-25 14:09:16 -0600

    2nd api call returns restaurant, name, address, lat, long, website, and photo urls... mmm.. food

--- api-test/api-call-test.js
+++ api-test/api-call-test.js
@@ -2,6 +2,7 @@
 
 const request = require('request');
 const myKey = 'AIzaSyDAe01cMlK4IWJMX4_KoTn9gSEKnfydK0M';
+//'AIzaSyD3essuc-XcBtyX5W4TroWXQLWOug2xb5o';
 
 let restaurantArray = [];
 
@@ -10,12 +11,11 @@ function getRestaurants() {
 	request("https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=39.758451,-105.00762450000002&rankby=distance&type=restaurant&key=" + myKey, function(error, response, body) {
 		console.log('error:', error); // Print the error if one occurred 
 		console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
-		console.log('body:', body); // Print the response body
+		//console.log('body:', body); // Print the response body
 		var parsedData = JSON.parse(body);
 		parsedData.results.forEach(function(restaurant) {
 			restaurantArray.push(restaurant.place_id);
 		});
-		//console.log(restaurantArray);
 		getRestaurantDetails();
 	});
 }
@@ -24,8 +24,8 @@ function getRestaurantDetails() {
 	console.log('this is the google places api call - details');
 	restaurantArray.forEach(function(restaurant) {
 		request("https://maps.googleapis.com/maps/api/place/details/json?placeid=" + restaurant + "&key=" + myKey, function(error, response, body) {
-			//console.log('error:', error); // Print the error if one occurred 
-			//console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
+			console.log('error:', error); // Print the error if one occurred 
+			console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received 
 			//console.log('body:', body); // Print the response body
 			var parsedData = JSON.parse(body);
 			console.log(parsedData.result.name);
@@ -33,25 +33,13 @@ function getRestaurantDetails() {
 			console.log(parsedData.result.geometry.location.lat);
 			console.log(parsedData.result.geometry.location.lng);
 			console.log(parsedData.result.website);
+			for (let i in parsedData.result.photos) {
+				if (parsedData.result.photos[i].photo_reference) {
+					console.log("https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=" + parsedData.result.photos[i].photo_reference + "&key=" + myKey);
+				}
+			}
 		});
 	});
 }
 
 getRestaurants();
-
-//function getphoto
-//https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=YOUR_API_KEY
-
-// function getLocation() {
-//   if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(showPosition);
-//   } else {
-//       console.log("Geolocation is not supported by this browser.");
-//   }
-// }
-
-// function showPosition(position) {
-//   console.log("Latitude: " + position.coords.latitude + " " + "Longitude: " + position.coords.longitude); 
-// }
-
-// getLocation();
