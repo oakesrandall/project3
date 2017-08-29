9ce0bacd68962bafb7492c1a51de39c6c71e3cdd
David_Randall <oakesrandall@gmail.com>
2017-08-29 10:39:38 -0600

    commiting so git will stop bothering me

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AuthService } from './auth.service';
 import { AppComponent } from './app.component';
 import { ImageComponent } from './image/image.component';
-<<<<<<< HEAD
 import { AboutComponent } from './about/about.component';
 import { LandingComponent } from './landing/landing.component';
-=======
 import { FormsModule } from '@angular/forms';
 import { ApiKeyService } from './apikey.service';
->>>>>>> 0ee768157fb76c9c39c3f039238b687827344018
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    ImageComponent,
-    AboutComponent,
-    LandingComponent
-=======
     ImageComponent
     
     
->>>>>>> 0ee768157fb76c9c39c3f039238b687827344018
   ],
   imports: [
     BrowserModule,
