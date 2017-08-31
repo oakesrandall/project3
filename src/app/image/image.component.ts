7e81c68d6381505c77602f8e427b58cf7f0ab295
David_Randall <oakesrandall@gmail.com>
2017-08-31 14:21:57 -0600

    last merge thursday

--- src/app/image/image.component.ts
+++ src/app/image/image.component.ts
@@ -15,10 +15,10 @@ import { Subscription } from 'rxjs/Subscription';
 
 export class ImageComponent implements OnInit {
 
-  favorites: Favorite[]; 
-  subscription: Subscription;
+    favorites: Favorite[]; 
+    subscription: Subscription;
 
-  currentImage: any = './assets/images/loading_burger.gif';
+    currentImage: any = './assets/images/loading_burger.gif';
     myKey: any = 'AIzaSyD3essuc-XcBtyX5W4TroWXQLWOug2xb5o';
     imageCounter: number = 0;
     arrayCounter: number = 0;
@@ -38,14 +38,14 @@ export class ImageComponent implements OnInit {
 
     getRestaurants() {
         console.log('this is the google places api call - nearby');
-    return this.http.get(this.googlePlacesNearbyAPIurl, this.options)
+        return this.http.get(this.googlePlacesNearbyAPIurl, this.options)
     .toPromise()
     .then(response => {
-      this.results = response.json().results;
+        this.results = response.json().results;
     })
     .then(response => {
-      this.results.forEach(restaurant => {
-        this.restaurantArray.push(restaurant.place_id);
+        this.results.forEach(restaurant => {
+            this.restaurantArray.push(restaurant.place_id);
       })
       console.log(this.restaurantArray);
       this.getRestaurantDetails();
@@ -53,33 +53,32 @@ export class ImageComponent implements OnInit {
   }
 
 getRestaurantDetails() {
-         console.log('this is the google places api call - details');
+     console.log('this is the google places api call - details');
      this.restaurantArray.forEach(restaurant => {
-       return this.http.get('https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/details/json?placeid=' + restaurant + '&key=' + this.myKey, this.options) 
-       .toPromise()
-       .then(response => {
+         return this.http.get('https://thingproxy.freeboard.io/fetch/https://maps.googleapis.com/maps/api/place/details/json?placeid=' + restaurant + '&key=' + this.myKey, this.options) 
+         .toPromise()
+         .then(response => {
          this.results = response.json().result;
          let tempArray = [];
          let restaurantObject = {
-           name: this.results.name,
-           address: this.results.formatted_address,
-           lat: this.results.geometry.location.lat,
-           lng: this.results.geometry.location.lng,
-           websiteURL: this.results.website,
-           photos: [],
+             name: this.results.name,
+             address: this.results.formatted_address,
+             lat: this.results.geometry.location.lat,
+             lng: this.results.geometry.location.lng,
+             websiteURL: this.results.website,
+             photos: [],
          };
          for (let i in this.results.photos) {
-           if (this.results.photos[i].photo_reference) {
-            tempArray.push('https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=' + this.results.photos[i].photo_reference + '&key=' + this.myKey);
-           }
+             if (this.results.photos[i].photo_reference) {
+             tempArray.push('https://maps.googleapis.com/maps/api/place/photo?maxwidth=800&photoreference=' + this.results.photos[i].photo_reference + '&key=' + this.myKey);
+             }
          }
          restaurantObject.photos = tempArray;
-         //console.log(restaurantObject.photos);
          this.restaurantObjectsForPassingArray.push(restaurantObject);
-       });
+         });
      });
-        console.log(this.restaurantObjectsForPassingArray);
-  }
+     console.log(this.restaurantObjectsForPassingArray);
+}
 
   constructor(private http: Http,
   			 private route: ActivatedRoute,
@@ -87,17 +86,17 @@ getRestaurantDetails() {
   			 private favoriteService: FavoritesService
   ) { }
 
-  SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight'};
-    swipe(action) {
-        if (action === this.SWIPE_ACTION.LEFT) {
-			  console.log('swiped left');
-			  this.getImage();
+    SWIPE_ACTION = { LEFT: 'swipeleft', RIGHT: 'swiperight'};
+        swipe(action) {
+            if (action === this.SWIPE_ACTION.LEFT) {
+			    console.log('swiped left');
+			    this.getImage();
             
-        } else if (action === this.SWIPE_ACTION.RIGHT) {
-			  console.log('swiped right')
-			  this.getImage();
+            } else if (action === this.SWIPE_ACTION.RIGHT) {
+			    console.log('swiped right')
+			    this.getImage();
+            }
         }
-  }
 
 
     clickYes() {
